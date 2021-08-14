7603e8dabd4e5a3f2c88dcf20ea59575cfaeeee3
MET-DEV <mustafatpe23@gmail.com>
2021-08-14 12:02:10 +0300

    first commit

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const Sequelize=require('sequelize')
+const db=require('../config/database.js')
+
+const User=db.define('users',{
+    first_name:{
+        type:Sequelize.STRING
+    },
+    last_name:{
+        type:Sequelize.STRING
+    },
+    email:{
+        type:Sequelize.STRING
+    },
+    password:{
+        type:Sequelize.STRING
+    },
+})
+module.exports=User
